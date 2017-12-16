c6b6708e7761a04055faca8d13a0e4c2660b4758
Patrick Dartora <patrickdartoralpt@gmail.com>
2017-12-16 12:57:49 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/patrickDartora/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.patrickDartora.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.patrickDartora.cursomc.domain.Categoria;
+import com.patrickDartora.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Categoria obj = repo.findOne(id);
+		
+		return obj;
+	}
+}
