bdaae6bd0a18a9f5f1d492f3b50838268ea810bd
Patrick Dartora <patrickdartoralpt@gmail.com>
2017-12-18 13:59:21 -0200

    Ajustes no endpoint /categoria/{id}

--- src/main/java/com/patrickDartora/cursomc/services/CategoriaService.java
+++ src/main/java/com/patrickDartora/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.patrickDartora.cursomc.domain.Categoria;
 import com.patrickDartora.cursomc.repositories.CategoriaRepository;
+import com.patrickDartora.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,6 +16,9 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", tipo: " + Categoria.class.getName());
+		}
 		
 		return obj;
 	}
